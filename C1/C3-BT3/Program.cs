bc7b3bb9c0484e145d577015aca1b09419f5976b
NhanNguyen <55469146+tinynguyen77@users.noreply.github.com>
2021-08-25 23:42:42 +0700

    bt3 ch1

---
+++ C1/C3-BT3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+internal class SockProp
+{
+    public static void Main()
+    {
+        IPAddress ia = IPAddress.Parse("127.0.0.1");
+        IPEndPoint ie = new IPEndPoint(ia, 8000);
+        Socket test = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Console.WriteLine("AddressFamily: {0}", test.AddressFamily);
+        Console.WriteLine("SocketType: {0}", test.SocketType);
+        Console.WriteLine("ProtocolType: {0}", test.ProtocolType);
+        Console.WriteLine("Blocking: {0}", test.Blocking);
+        test.Blocking = false;
+        Console.WriteLine("new Blocking: {0}", test.Blocking);
+        Console.WriteLine("Connected: {0}", test.Connected);
+        test.Bind(ie);
+        IPEndPoint iep = (IPEndPoint)test.LocalEndPoint;
+        Console.WriteLine("Local EndPoint: {0}",
+        iep.ToString());
+        test.Close();
+    }
+}
