ce16543d41987e94e960e765548eecdf49846319
Nhân Nguyễn <nhantrongnguyen147@gmail.com>
2021-09-03 12:54:19 +0700

    update comment C2

--- C2/C2bt1.2/C2bt12.cs
+++ C2/C2bt1.2/C2bt12.cs
@@ -8,9 +8,13 @@ class SimpleTcpClient
     public static void Main()
     {
         byte[] data = new byte[1024];
+        // tạo bộ đếm
         string input, stringData;
+        // tạo biến 
         IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        // tạo đối tượng ipep là IPEndPorn có giá trị là 127.0.0.1:9050
         Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        // tạo sockket mới để kết nối với server trên tầng vận chuyển
         try
         {
             server.Connect(ipep);
@@ -21,21 +25,33 @@ class SimpleTcpClient
             Console.WriteLine(e.ToString());
             return;
         }
-
+        // cho socket "server" của client kết nối tới server
+        // nếu thất bại thì xuất thông báo, thành công thì làm tiếp phía dưới
         var recv = server.Receive(data);
+        // khai báo biến recv để nhận dữ liệu được gửi từ server qua socket "server"
         stringData = Encoding.ASCII.GetString(data, 0, recv);
+        // convert data đã nhận từ server sang dạng chuỗi và gán vào stringData
         Console.WriteLine(stringData);
+        // xuất ra màn hình dữ liệu trong stringData
+        // thực hiện vòng lặp:
         while (true)
         {
             input = Console.ReadLine();
             if (input == "exit")
                 break;
+            // nếu nhập exit thì thoát vòng lặp, disconnect server, tắt socket
+            // nếu data != exit thì thực hiện tiếp bên dưới
             server.Send(Encoding.ASCII.GetBytes(input));
+            // dữ liệu đc convert thành dạng byte và gửi đến server
             data = new byte[1024];
+            // tạo bộ đếm mới
             recv = server.Receive(data);
+            // đưa dữ liệu đã nhận tự server vào recv
             stringData = Encoding.ASCII.GetString(data, 0, recv);
+            // convert data thành dạng chuỗi, gán vào stringData
             Console.WriteLine(stringData);
         }
+        
 
         Console.WriteLine("Disconnecting from server ...");
         server.Shutdown(SocketShutdown.Both);
--- C2/C2bt1/C2bt11.cs
+++ C2/C2bt1/C2bt11.cs
@@ -12,32 +12,39 @@ class SimpleTcpSrvr
         byte[] data = new byte[1024];
         // tạo mảng data 
         IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
-        // tạo đối tượng ipep là IPEndPoint có giá trị là địa chỉ ip bất kì chấp nhận port 9050
+        // tạo đối tượng ipep là IPEndPoint có giá trị là địa chỉ ip bất kì ở port 9050
         Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-        // tạo socket mới
+        // tạo socket mới để kết nối tới client trong tầng vận chuyển
         newsock.Bind(ipep);
         // gán ipep vào newsock
         newsock.Listen(10);
         // chuyển newsock sang trạng thái lắng nghe tối đa 10 kết nối
         Console.WriteLine("Waiting for a client ... ");
+        // xuất ra màn hình khi khởi chạy server
         Socket client = newsock.Accept();
-        // khởi tạo socket client bên phía server để giao tiếp với client
+        // khởi tạo socket client bên phía server để lắng nghe giao tiếp với client
         IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
         // khởi tạo dối tượng clientep là IPEndPoint và kết nối tới socket client
         Console.WriteLine("Connected with {0} at port {1}", clientep.Address, clientep.Port);
+        // xuất ra màn hình IPaddress và port của clientep
         string welcome = ("Welcome to my test server");
         data = Encoding.ASCII.GetBytes(welcome);
+        //gán chuỗi welcome vào data dưới dạng byte
         client.Send(data, data.Length, SocketFlags.None);
         // gửi toàn bộ data
         while (true)
         {
             data = new byte[1024];
-            recv = client.Receive(data); // đối tượng recv nhận data được gửi vào từ client
+            // tạo mảng data mới 
+            recv = client.Receive(data); 
+            // đối tượng recv nhận data gửi "client" từ phía client
             if (recv == 0)
                 break;
+            // nếu k có data nhận vào thì thoát vòng lặp
             Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv)); 
-            // xuất ra data đã nhận ở dạng cuỗi
+            // xuất ra data đã nhận ở dạng chuỗi và đặt vào mảng data
             client.Send(data, recv, SocketFlags.None);
+            // gửi dữ liệu từ recv trong mảng
             
         }
 
